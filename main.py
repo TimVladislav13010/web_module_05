732add748ed72f539352ba8d339aa8f453db9df4
Vladislav <109065019+TimVladislav13010@users.noreply.github.com>
2023-02-17 21:51:16 +0200

    add additional currency

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ currency_value = ["USD",
                   "EUR"]
 
 
-def valid_urls(url, count_day):
+def valid_urls(url: str, count_day: int) -> list:
     now_day = datetime.now()
     new_url = list()
     new_url.append(f"{url + now_day.strftime('%d.%m.%Y')}")
@@ -39,18 +39,36 @@ def valid_urls(url, count_day):
     return new_url
 
 
-def handler_json(json_text):
+def handler_json(json_text: dict) -> dict:
     result = dict()
     for exchange_rate in json_text["exchangeRate"]:
         if len(result) == 0:
             result.update([(json_text["date"], dict())])
         if exchange_rate["currency"] in currency_value:
-            currency = {exchange_rate["currency"]: {"sale": exchange_rate["saleRate"], "purchase": exchange_rate["purchaseRate"]}}
-            result[json_text["date"]].update(currency)
+            currency_ = {exchange_rate["currency"]: {"sale": exchange_rate["saleRate"], "purchase": exchange_rate["purchaseRate"]}}
+            result[json_text["date"]].update(currency_)
     return result
 
 
-async def request(url, session):
+def handler_arg_currency(value: str) -> list | None:
+    value = value.upper()
+    value = value.replace(' ', '')
+    currency_arg = value.split(",")
+
+    if isinstance(currency_arg, list):
+        for cur in currency_arg:
+            if len(cur) != 3:
+                logger.error(f"Wrong currency: {cur}. Currency -> USD,EUR....")
+                return None
+
+    elif len(currency_arg) != 3:
+        logger.error(f"Wrong currency: {currency_arg}. Currency -> USD,EUR....")
+        return None
+
+    return currency_arg
+
+
+async def request(url: str, session: aiohttp.ClientSession) -> dict | None:
     try:
         async with session.get(url) as response:
             if response.status == 200:
@@ -63,11 +81,19 @@ async def request(url, session):
     return None
 
 
-async def main(value=days):
+async def main(value=days) -> str:
+    if currency:
+        global currency_value
+        currency_arg = handler_arg_currency(currency)
+        if currency_arg:
+            currency_value = [*currency_value, *currency_arg]
+        logger.info(f"Show currency: {currency_value}")
+
     if value > 10 or value < 1:
         result = f"Please enter valid days 1....10"
         logger.error(result)
         return result
+
     else:
         urls = valid_urls(URL, value)
         async with aiohttp.ClientSession() as session:
